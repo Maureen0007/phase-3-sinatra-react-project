9303a6cbcd6fcbdb4094036514d4891b3379162e
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-09-20 02:21:03 +0300

    change image path

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,16 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 # book
-Book.create(name:"Milk and honey", author_id:3 , image:"https://unsplash.com/photos/CXYPfveiuis")
-Book.create(name:"Alex Rider", author_id:1 , image:"https://unsplash.com/photos/dBFF-g2MhoE")
-Book.create(name:"Your heart is the sea", author_id:5 , image:"https://unsplash.com/photos/B1dQCWz0U2w")
-Book.create(name:"Beloved", author_id:1 , image:"https://unsplash.com/photos/9dXSoi6VXEA")
-Book.create(name:"The psychology of money", author_id:4 , image:"https://unsplash.com/photos/aZ_MmSmAcjg")
-Book.create(name:"Mad about the boy", author_id:2 , image:"https://unsplash.com/photos/_LzFAUyqVtg")
-Book.create(name:"The two towers", author_id:2 , image:"https://unsplash.com/photos/O7ygzpAL4Mc")
-Book.create(name:"Your soul is a River", author_id:5 , image:"https://unsplash.com/photos/DxAzOKSiPoE")
-Book.create(name:"Heated", author_id:4 , image:"https://unsplash.com/photos/Gls5DB9lk6s")
-Book.create(name:"How Innovation Works", author_id:1 , image:"https://unsplash.com/photos/H-LIL57PHCc")
+Book.create(name:"Milk and honey", author_id:3 , image:"https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80")
+Book.create(name:"Alex Rider", author_id:1 , image:"https://images.unsplash.com/photo-1598618253208-d75408cee680?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80")
+Book.create(name:"Your heart is the sea", author_id:5 , image:"https://images.unsplash.com/photo-1555252586-d77e8c828e41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80")
+Book.create(name:"Beloved", author_id:1 , image:"https://images.unsplash.com/photo-1610882648335-ced8fc8fa6b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80")
+Book.create(name:"The psychology of money", author_id:4 , image:"https://images.unsplash.com/photo-1592496431122-2349e0fbc666?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=912&q=80")
+Book.create(name:"Mad about the boy", author_id:2 , image:"https://images.unsplash.com/photo-1521105993401-3a51411aff9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")
+Book.create(name:"The two towers", author_id:2 , image:"https://images.unsplash.com/photo-1621351183012-e2f9972dd9bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80")
+Book.create(name:"Your soul is a River", author_id:5 , image:"https://images.unsplash.com/photo-1511108690759-009324a90311?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80")
+Book.create(name:"The Subtle Art Of Not Giving A Fuck", author_id:4 , image:"https://images.unsplash.com/photo-1621827979802-6d778e161b28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80")
+Book.create(name:"How Innovation Works", author_id:1 , image:"https://images.unsplash.com/photo-1589829085413-56de8ae18c73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=912&q=80")
 
 # chapter
 Chapter.create(title:"The pilot", description:"This is the introduction and first act of the book", book_id:1)
